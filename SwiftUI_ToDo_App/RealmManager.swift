d9acd3b6a8221dea1b596de63191cd5e2efd4ec8
Stanley Pan <imoment.developer@gmail.com>
2021-12-19 17:05:25 -0500

    Added Realm and RealmSwift dependency

---
+++ SwiftUI_ToDo_App/RealmManager.swift
@@ -0,0 +1,13 @@
+//
+//  RealmManager.swift
+//  SwiftUI_ToDo_App
+//
+//  Created by Stanley Pan on 2021/12/19.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    
+}
--- SwiftUI_ToDo_App/Views/ContentView.swift
+++ SwiftUI_ToDo_App/Views/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAddTaskView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottomTrailing) {
+            TasksView()
+            
+            SmallAddButton()
+                .padding()
+                .onTapGesture {
+                    showAddTaskView.toggle()
+                }
+        }
+        .sheet(isPresented: $showAddTaskView) {
+            AddTaskView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
     }
 }
 
