6ebd0d7d68a7fa5d950f0bcf00ab0375e0e75ab4
Stanley Pan <stanleypan.egre@gmail.com>
2021-12-19 15:39:56 -0500

    Initial Commit

---
+++ SwiftUI_ToDo_App/SwiftUI_ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDo_AppApp.swift
+//  SwiftUI_ToDo_App
+//
+//  Created by Stanley Pan on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
